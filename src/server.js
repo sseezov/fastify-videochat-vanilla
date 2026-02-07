940a44c713a899496ce2d11c7348d000e634d5ee
sseezov <sseezov@mail.ru>
2026-02-07 10:42:05 +0700

    add pug

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
-import fastifyStatic from '@fastify/static';
+import fastifyView from '@fastify/view';
 import path from 'node:path'
+import pug from 'pug'
 import { v4 as uuidv4 } from 'uuid';
 
 const __dirname = import.meta.dirname;
@@ -9,12 +10,19 @@ const fastify = Fastify({
   logger: true
 })
 
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, '../public'),
+fastify.register(fastifyView, {
+  engine: {
+    pug
+  },
+  root: path.join(__dirname, 'views')
 });
 
 fastify.get('/', (request, reply) => {
-  reply.sendFile('index.html');
+  // Renders the 'home.pug' file located in the 'views' directory
+  reply.view('home.pug', { 
+    name: 'World',
+    title: 'Fastify Pug Example'
+  });
 });
 
 fastify.get('/:room', (request, reply) => {
