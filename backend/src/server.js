789eb59466a1c42837f32d4183a0a085322f1d29
sseezov <sseezov@mail.ru>
2026-02-07 10:18:30 +0700

    init

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import Fastify from 'fastify'
+import fastifyStatic from '@fastify/static';
+import path from 'node:path'
+
+const __dirname = import.meta.dirname;
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, '../public'),
+});
+
+fastify.get('/', (request, reply) => {
+  reply.sendFile('index.html');
+});
+
+// fastify.get('/apiv1/teachers', (req, reply) => {
+//   reply.send(teachers)
+// })
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
